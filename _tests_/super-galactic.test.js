535898b2661500b810a4130d04c7005bc2d5171b
wymerkd <wymerkd@gmail.com>
2019-11-15 11:06:11 -0800

    add updated test for calculating age on Mercury, add function to super-galactic.js to calculate age on Mercury.

--- _tests_/super-galactic.test.js
+++ _tests_/super-galactic.test.js
@@ -3,18 +3,17 @@ import { Age } from './../src/super-galactic.js';
 describe('Age', () => {
 
   test('should correctly create an age object with current age', () => {
-    var userAge = new Age(29);
+    let userAge = new Age(29);
     expect(userAge.earthYears).toEqual(29);
   });
 
   test('should correctly determine if age type is a number', () => {
-    var ageType = new Age(29);
+    let ageType = new Age(29);
     expect(typeof ageType.earthYears).toEqual('number');
   });
 
   test('should correctly divide user age by .24 to determine age on Mercury', () => {
     var userAge = new Age(29);
-    expect(userAge.earthYears.mercuryAge()).toEqual(121);
+    expect(userAge.mercuryAge()).toEqual(121);
   });
-
 });
--- src/super-galactic.js
+++ src/super-galactic.js
@@ -2,4 +2,9 @@ export class Age {
   constructor(earthYears) {
     this.earthYears = earthYears;
   }
-}
+
+  mercuryAge() {
+    let mercury = Math.round(this.earthYears / .24);
+    return mercury;
+  }
+};
