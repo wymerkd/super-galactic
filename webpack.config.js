faac29b41207d953a75e82c620ee80bce6f6574c
wymerkd <wymerkd@gmail.com>
2019-11-15 08:38:18 -0800

    add package.json and update webpack with necessary plugins.

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'project-name',
+      title: 'super-galactic',
       template: './src/index.html',
       inject: 'body'
     })
