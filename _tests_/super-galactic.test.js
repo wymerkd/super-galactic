81b8c0f4c715a88dd562c14b1227b53a07c4389b
wymerkd <wymerkd@gmail.com>
2019-11-15 13:11:49 -0800

    remove refactoring attempts.

--- _tests_/super-galactic.test.js
+++ _tests_/super-galactic.test.js
@@ -13,22 +13,22 @@ describe('Age', () => {
   });
 
   test('should correctly divide user age by .24 to determine age on Mercury', () => {
-    var userAge = new Age(29);
+    let userAge = new Age(29);
     expect(userAge.mercuryAge()).toEqual(121);
   });
 
   test('should correctly divide user age by .62 to determine age on Venus', () => {
-    var userAge = new Age(29);
+    let userAge = new Age(29);
     expect(userAge.venusAge()).toEqual(47);
   });
 
   test('should correctly divide user age by 1.88 to determine age on Mars', () => {
-    var userAge = new Age(29);
+    let userAge = new Age(29);
     expect(userAge.marsAge()).toEqual(15);
   });
 
   test('should correctly divide user age by 11.86 to determine age on Jupiter', () => {
-    var userAge = new Age(29);
+    let userAge = new Age(29);
     expect(userAge.jupiterAge()).toEqual(2);
   });
 });
--- src/super-galactic.js
+++ src/super-galactic.js
@@ -1,6 +1,7 @@
 export class Age {
   constructor(earthYears) {
     this.earthYears = earthYears;
+    this.jupiterYears = [];
   }
 
   mercuryAge() {
