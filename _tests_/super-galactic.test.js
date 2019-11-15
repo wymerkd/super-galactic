5566cc432abcf251d1d03b0819c541d8727a9094
wymerkd <wymerkd@gmail.com>
2019-11-15 13:45:59 -0800

    refactor code in each test by removing repetitive userAge variable and declaring userAge before tests.

--- _tests_/super-galactic.test.js
+++ _tests_/super-galactic.test.js
@@ -9,27 +9,22 @@ describe('Age', () => {
   });
 
   test('should correctly determine if age type is a number', () => {
-    let ageType = new Age(29);
-    expect(typeof ageType.earthYears).toEqual('number');
+    expect(typeof userAge.earthYears).toEqual('number');
   });
 
   test('should correctly divide user age by .24 to determine age on Mercury', () => {
-    let userAge = new Age(29);
     expect(userAge.mercuryAge()).toEqual(121);
   });
 
   test('should correctly divide user age by .62 to determine age on Venus', () => {
-    let userAge = new Age(29);
     expect(userAge.venusAge()).toEqual(47);
   });
 
   test('should correctly divide user age by 1.88 to determine age on Mars', () => {
-    let userAge = new Age(29);
     expect(userAge.marsAge()).toEqual(15);
   });
 
   test('should correctly divide user age by 11.86 to determine age on Jupiter', () => {
-    let userAge = new Age(29);
     expect(userAge.jupiterAge()).toEqual(2);
   });
 });
