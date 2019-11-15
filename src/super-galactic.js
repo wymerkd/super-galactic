4a7b17b80b9f7f95e22c17647b23b1e1377645e6
wymerkd <wymerkd@gmail.com>
2019-11-15 12:07:34 -0800

    add code to super-galactic.js to calculate user age on Jupiter.

--- src/super-galactic.js
+++ src/super-galactic.js
@@ -7,4 +7,19 @@ export class Age {
     let mercury = Math.round(this.earthYears / .24);
     return mercury;
   }
+
+  venusAge() {
+    let venus = Math.round(this.earthYears / .62);
+    return venus;
+  }
+
+  marsAge() {
+    let mars = Math.round(this.earthYears / 1.88);
+    return mars;
+  }
+
+  jupiterAge() {
+    let jupiter = Math.round(this.earthYears / 11.86);
+    return jupiter;
+  }
 };
