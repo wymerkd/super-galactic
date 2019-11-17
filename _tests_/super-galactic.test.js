948b521dee2e15fa18528b0ad79de76aa1c10589
wymerkd <wymerkd@gmail.com>
2019-11-17 10:38:05 -0800

    add code to return life left on Mars.

--- _tests_/super-galactic.test.js
+++ _tests_/super-galactic.test.js
@@ -42,4 +42,8 @@ describe('Age', () => {
   test('should correctly subtract user age from life expectancy on Earth and divide result by .62 to determine life left on Venus', () => {
     expect(userAge.venusLife()).toEqual(68);
   });
+
+  test('should correctly subtract user age from life expectancy on Earth and divide result by 1.88 to determine life left on Mars', () => {
+    expect(userAge.marsLife()).toEqual(22);
+  });
 });
