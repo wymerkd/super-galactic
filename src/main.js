363030e9ef27ef28fc0d0cf6bc248c34790acbbc
wymerkd <wymerkd@gmail.com>
2019-11-17 12:20:31 -0800

    import Age class

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
-import $ from 'jquery';
-import 'bootstrap';
-import './styles.css';
+// import $ from 'jquery';
+// import 'bootstrap';
+// import './styles.css';
+// import { Age } from './../src/super-galactic.js';
